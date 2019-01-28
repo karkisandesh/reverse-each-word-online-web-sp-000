6c6b2c68197f7f071c83826d3c095ebea95aa629
Sandesh Karki <karkisandesh@hotmail.com>
2019-01-28 00:32:59 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -9,7 +9,7 @@ def reverse_each_word(sentence)
   array = sentence.split(" ")
   rev_array = [ ] 
   array.collect do |item|
-  rev_array.push("#{item.reverse}")
-  rev_array.join(" ")  
-end
+    rev_array.push("#{item.reverse}")
+    rev_array.join(" ")  
+  end
 end
